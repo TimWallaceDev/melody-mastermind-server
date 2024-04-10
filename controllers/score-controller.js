030e14f74b9beaac3ebdaa8a795438c19ac9b8ab
Tim Wallace <timwallacedev@gmail.com>
2024-04-10 16:19:26 -0400

    create scores endpoints => POST score and GET playlist scores

--- controllers/score-controller.js
+++ controllers/score-controller.js
@@ -1,19 +1,18 @@
-// import Knex from "knex";
-// import {Knexfile} from "../knexfile.js";
-// const knex = Knex(Knexfile.development);
+import Knex from "knex";
+import Knexfile from "../knexfile.js";
+const knex = Knex(Knexfile.development);
 
 
 export async function playlistScores(req, res){
-
+    const {playlist} = req.params
+    console.log(playlist)
     try {
         //get scores for individual playlist
-        //get playlist name
-        const {playlist} = req.params
-
-        //get playlist scores with Knex
+        const scores = await knex("scores").select("*").where({playlist_id: playlist})
+        console.log(scores)
 
         //return results to client
-        res.status(200).send("here is the scores for an individual playlist")
+        res.status(200).json(scores)
     }
     catch(err){
         console.log(err)
@@ -22,12 +21,15 @@ export async function playlistScores(req, res){
 }
 
 export async function postScore(req, res){
-
+    const {username, score, playlist_id} = req.body
+    console.log(username, score, playlist_id)
     try {
+        //get user id
+        const userArr = await knex("users").select("id").where({username: username})
+        const user_id = userArr[0].id
+        console.log("user id:", user_id)
         //save score to database
-        //get information from body of request
-
-        //save score into database
+        const response = await knex("scores").insert({user_id: user_id, score, playlist_id: playlist_id})
 
         //send confirmation
         res.status(201).send("score has been saved in the database")
