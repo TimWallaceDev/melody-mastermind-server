d01192fe3ace5fd91431c05b1fc90d9dbe2973a8
Tim Wallace <timwallacedev@gmail.com>
2024-05-07 15:03:37 -0400

    update seed data

--- seeds/03_scores.js
+++ seeds/03_scores.js
@@ -6,15 +6,15 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('scores').del()
   await knex('scores').insert([
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 5},
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 4},
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 6},
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 6},
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 2},
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 4},
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 5},
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 8},
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 9},
-    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 13},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 5504},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 4503},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 6675},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 6902},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 2123},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 4123},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 5534},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 8093},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 9387},
+    {user_id: 3, playlist_id: '5ABHKGoOzxkaa28ttQV9sE', score: 1302},
   ]);
 };
