af3605125d82fe7b7a9d57a6653893b666340935
Tim Wallace <timwallacedev@gmail.com>
2024-05-21 10:12:13 -0400

    fix backend urls for deployment

--- server.js
+++ server.js
@@ -15,17 +15,17 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/token", tokenRouter)
+app.use("/melody-mastermind/api/token", tokenRouter)
 
-app.use("/api/scores", scoreRouter)
+app.use("/melody-mastermind/api/scores", scoreRouter)
 
-app.use("/api/users", userRouter)
+app.use("/melody-mastermind/api/users", userRouter)
 
-app.use("/api/account", accountRouter)
+app.use("/melody-mastermind/api/account", accountRouter)
 
-app.use("/api/playlists", playlistRouter)
+app.use("/melody-mastermind/api/playlists", playlistRouter)
 
 
 app.listen(PORT, () => {
-    console.log(`running on http://localhost:${PORT}/api`);
+    console.log(`running on http://localhost:${PORT}/melody-mastermind/api`);
 })
