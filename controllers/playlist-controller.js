4e262d9d5d8e6cb8da6f493684cbab7e40c70d01
Tim Wallace <timwallacedev@gmail.com>
2024-05-27 12:54:02 -0400

    sort playlists by popularity

--- controllers/playlist-controller.js
+++ controllers/playlist-controller.js
@@ -3,13 +3,18 @@ import Knexfile from "../knexfile.js";
 const knex = Knex(Knexfile.development);
 
 
-export async function getPlaylists(req, res){
+export async function getPlaylists(req, res) {
 
     try {
-        const playlists = await knex("playlists").select("*")
+        const playlists = await knex('playlists')
+            .leftJoin('scores', 'playlists.id', 'scores.playlist_id')
+            .select('playlists.id', 'playlists.name', 'playlists.image_url',)
+            .count('scores.id as score_count')
+            .groupBy('playlists.id', 'playlists.name')
+            .orderBy('score_count', 'desc');
         res.status(200).json(playlists)
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
         res.status(500).send("Something has gone awry")
     }
